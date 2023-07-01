2fc9a8c43f57ee93f373de99ee95f4a9c8f46685
Nikolay <dojazz@icloud.com>
2023-07-01 11:50:21 +0300

    added AlLure Report edited build

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -7,8 +7,9 @@ public class CalendarComponent {
     public void setDate(String day, String month, String year) {
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker__day--0" + day +
-                ":not(.react-datepicker__day--outside-month)").click();
+        $(".react-datepicker__day--0" + day).click();
+/*        $(".react-datepicker__day--0" + day +
+                ":not(.react-datepicker__day--outside-month)").click(); */
         }
     }
 
