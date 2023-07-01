a99e1ec2f547389a19b3088c1e4449a0688106ae
Nikolay <dojazz@icloud.com>
2023-07-01 12:15:09 +0300

    added AlLure Report edited build

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,7 +6,10 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.DemoqaPracticeFormPage;
+
+import java.util.Map;
 
 public class TestBase {
     TestData testData = new TestData();
@@ -16,10 +19,19 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
+        // Configuration.pageLoadTimeout = 50000;
+
         Configuration.browserSize = "1920x1080";
-       // Configuration.pageLoadTimeout = 50000;
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+                ));
+        Configuration.browserCapabilities = capabilities;
 
 
 /*        ChromeOptions options = new ChromeOptions();
